12f7217c79229f127e44b8210f1429534502c11b
unknown <sashakotik450697@gmail.com>
2024-07-30 19:05:56 +0300

    Add tracks data seed

--- SpotifyAPI/Seeder/DataSeeder.cs
+++ SpotifyAPI/Seeder/DataSeeder.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Model.Context;
 using Model.Entities;
+using Newtonsoft.Json;
 using SpotifyAPI.Seeder.Interfaces;
 using SpotifyAPI.Services.Interfaces;
 
@@ -13,6 +14,9 @@ public class DataSeeder(
     IAudioService audioService
     ) : IDataSeeder
 {
+    string apiKey = "2d375f10";
+    string id = "1248";
+
     public async Task SeedAsync()
     {
         if (!await context.Genres.AnyAsync())
@@ -98,6 +102,91 @@ public class DataSeeder(
 
         await context.Albums.AddRangeAsync(albums);
         await context.SaveChangesAsync();
+
+        await CreateTracksAsync(albums);
+    }
+
+    public async Task CreateTracksAsync(IList<Album> albums)
+    {
+        Faker faker = new Faker();
+        using var httpClient = new HttpClient();
+        var genres = await context.Genres.ToListAsync();
+
+        var tracks = new List<Track>();
+
+        foreach (var album in albums)
+        {
+            int count = faker.Random.Int(1, 10);
+
+            List<string> urls = await GetTrackUrlsAsync(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                var audioBytes = await httpClient.GetByteArrayAsync(urls[i]);
+                var audioPath = await audioService.SaveAudioAsync(audioBytes);
+
+                var track = new Track
+                {
+                    Name = faker.Lorem.Sentence(),
+                    Path = await audioService.SaveAudioAsync(audioBytes),
+                    Duration = audioService.GetAudioDuration(audioPath),
+                    AlbumId = album.Id
+                };
+                int genreCount = faker.Random.Int(1, 3);
+                var selectedGenres = faker.PickRandom(genres, genreCount).ToList();
+
+                track.Genres = selectedGenres.Select(g => new TrackGenre
+                {
+                    GenreId = g.Id,
+                    Track = track
+                }).ToList();
+
+                tracks.Add(track);
+            }
+        }
+
+        await context.Tracks.AddRangeAsync(tracks);
+        await context.SaveChangesAsync();
+    }
+    public async Task<List<string>> GetTrackIdsAsync(int count)
+    {
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync($"https://api.jamendo.com/v3.0/tracks/?client_id={apiKey}&format=json&limit={count}");
+        var json = await response.Content.ReadAsStringAsync();
+
+        var trackIds = new List<string>();
+        dynamic? data = JsonConvert.DeserializeObject(json);
+
+        if (data != null)
+        {
+            foreach (var track in data.results)
+            {
+                if (track.id != null)
+                {
+                    trackIds.Add((string) track.id);
+                }
+            }
+        }
+
+        return trackIds;
+    }
+
+    public async Task<List<string>> GetTrackUrlsAsync(int count)
+    {
+        using var httpClient = new HttpClient();
+        var trackIds = await GetTrackIdsAsync(count);
+        var trackUrls = new List<string>();
+
+        foreach (var trackId in trackIds)
+        {
+            var fileUrl = $"https://api.jamendo.com/v3.0/tracks/file/?client_id={apiKey}&id={trackId}";
+            if (!string.IsNullOrEmpty(fileUrl))
+            {
+                trackUrls.Add(fileUrl);
+            }
+        }
+
+        return trackUrls;
     }
 
     private static async Task<string> GetImageAsBase64Async(HttpClient httpClient, string imageUrl)
