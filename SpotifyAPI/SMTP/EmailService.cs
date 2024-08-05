242a7f64e3b2d23b78fb3194275815fdea9e8492
unknown <sashakotik450697@gmail.com>
2024-08-05 18:06:33 +0300

    Add SMTP

---
+++ SpotifyAPI/SMTP/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace SpotifyAPI.SMTP;
+
+public class EmailService(
+    IOptions<EmailConfiguration> options
+    ) : IEmailService
+{
+    private readonly EmailConfiguration emailConfiguration = options.Value;
+    public async Task SendAsync(Message messageData)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("MusicFlowMail", emailConfiguration.From));
+        message.To.Add(new MailboxAddress(messageData.Name, messageData.To));
+        message.Subject = "Reset Password";
+
+        message.Body = new TextPart("html")
+        {
+            Text = messageData.Body
+        };
+
+        using (var client = new SmtpClient())
+        {
+            try
+            {
+                await client.ConnectAsync(emailConfiguration.SmtpServer, emailConfiguration.Port, SecureSocketOptions.SslOnConnect);
+                await client.AuthenticateAsync(emailConfiguration.UserName, emailConfiguration.Password);
+                await client.SendAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending: " + ex);
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
+                client.Dispose();
+            }
+        }
+    }
+}
