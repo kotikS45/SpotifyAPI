8e0d8a365d1ee8df22eb83e0c497830d8df8a679
DanyloMartyniuk <73065591+DanyloMartyniuk@users.noreply.github.com>
2024-09-05 17:40:39 +0300

    Update PlaylistCreateVm.cs

--- SpotifyAPI/Models/Playlist/PlaylistCreateVm.cs
+++ SpotifyAPI/Models/Playlist/PlaylistCreateVm.cs
@@ -4,4 +4,5 @@ public class PlaylistCreateVm
 {
     public string Name { get; set; } = null!;
     public IFormFile Image { get; set; } = null!;
+    // public strinng Musec { get; get; } = null;
 }
