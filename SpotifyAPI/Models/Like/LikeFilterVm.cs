9d159473e7fe2e0574490484e72b6997feee4224
DanyloMartyniuk <73065591+DanyloMartyniuk@users.noreply.github.com>
2024-10-06 19:14:42 +0300

    Update LikeFilterVm.cs

--- SpotifyAPI/Models/Like/LikeFilterVm.cs
+++ SpotifyAPI/Models/Like/LikeFilterVm.cs
@@ -7,3 +7,21 @@ public class LikeFilterVm : PaginationVm
     public string? Name { get; set; }
     public string Username { get; set; } = null!;
 }
+/*
+namespace SpotifyAPI.Models.Follower
+{
+    public class FollowerFilterVm : BaseFilterVm
+    {
+        // Можна додати додаткові поля для специфічних фільтрів по Follower
+    }
+}
+
+namespace SpotifyAPI.Models.Like
+{
+    public class LikeFilterVm : BaseFilterVm
+    {
+        // Можна додати додаткові поля для специфічних фільтрів по Like
+    }
+}
+
+*/
