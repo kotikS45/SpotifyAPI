1cfb90458f1c9e360bc2f5f8067c47791cc22ac6
DanyloMartyniuk <73065591+DanyloMartyniuk@users.noreply.github.com>
2024-10-06 19:14:33 +0300

    Update FollowerVm.cs

--- SpotifyAPI/Models/Follower/FollowerVm.cs
+++ SpotifyAPI/Models/Follower/FollowerVm.cs
@@ -4,3 +4,14 @@ public class FollowerVm
 {
     public long ArtistId { get; set; }
 }
+/*
+namespace SpotifyAPI.Models.Follower
+{
+    public class FollowerVm
+    {
+        public long ArtistId { get; set; }
+        // Якщо є інші деталі про follower, їх можна додати сюди.
+    }
+}
+
+*/
