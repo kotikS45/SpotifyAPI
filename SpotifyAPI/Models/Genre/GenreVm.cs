949671c2f29f417920a8570eaad4668561341b25
DanyloMartyniuk <73065591+DanyloMartyniuk@users.noreply.github.com>
2024-10-06 19:14:39 +0300

    Update GenreVm.cs

--- SpotifyAPI/Models/Genre/GenreVm.cs
+++ SpotifyAPI/Models/Genre/GenreVm.cs
@@ -5,3 +5,25 @@ public class GenreVm
     public long Id { get; set; }
     public string Name { get; set; } = null!;
 }
+/*
+namespace SpotifyAPI.Models.Genre
+{
+    public class GenreCreateVm
+    {
+        public string Name { get; set; } = null!;
+    }
+
+    public class GenreUpdateVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class GenreVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
+
+*/
